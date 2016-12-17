a14a220fee0b771ca8de418011f89ac0b198c354
Kira <omitomi@yandex.ru>
2016-12-18 00:28:46 +0300

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -3,17 +3,10 @@ ruby '2.2.5'
 #ruby-gemset=railstutorial_rails_4_0
 
 gem 'rails', '4.0.2'
-
-group :development, :test do
-  gem 'sqlite3', '1.3.8'
-  gem 'rspec-rails', '2.13.1'
-end
-
-group :test do
-  gem 'selenium-webdriver', '2.35.1'
-  gem 'capybara', '2.1.0'
-end
-
+gem 'bootstrap-sass', '2.3.2.0'
+gem 'sqlite3'
+gem 'rake', '10.4.2'
+gem 'bcrypt-ruby'
 gem 'sass-rails', '4.0.1'
 gem 'uglifier', '2.1.1'
 gem 'coffee-rails', '4.0.1'
--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "sprockets/railtie"
+require "bootstrap-sass"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -13,6 +14,7 @@ Bundler.require(:default, Rails.env)
 
 module SampleApp
   class Application < Rails::Application
+    #config.secret_key_base = 'blipblapblup'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -24,5 +26,7 @@ module SampleApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif) 
+    #обеспечивает совместимость bootstrap-sass с файлопроводом
   end
 end
